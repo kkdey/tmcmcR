dec0c83c43e6521d177edf450f2548983360b2da
Kushal K Dey <kshldey@gmail.com>
2016-01-11 21:36:25 -0600

    added multiple try rwmh code

--- R/multry_rwmh_metrop.R
+++ R/multry_rwmh_metrop.R
@@ -1,6 +1,6 @@
-#' @title Simulate a Multiple Try Transformation based Markov Chain Monte Carlo (MT-TMCMC) algorithm (Rcpp sped up version)
+#' @title Simulate a Multiple Try RandomWalk Metropolis Hastings (MT-RWMH) algorithm (Liu, Liang and Wong 2000)
 #'
-#' @description The function simulates a Multiple try TMCMC (MT-TMCMC) chain of length nsamples using the scale, base and
+#' @description The function simulates a Multiple try RWMH (MT-RWMH) chain of length nsamples using the scale, base and
 #' burn in taken optimally as default or specified by user. The function outputs the full chain as well as the estimated
 #' posterior mean estimated from the samples drawn post burn-in.
 #'
@@ -8,10 +8,8 @@
 #' @param scale The proposal density scaling parameter. An approximation of the optimal scaling given the target_pdf is performed by OptimalScaling().
 #'              The default scale is this estimated optimal scaling
 #' @param base The starting value of the chain
-#' @param nmove_size The number of distinct move sizes used.
 #' @param nmove The total number of moves proposed.
 #' @param nsamples The number of samples to be drawn using the TMCMC algorithm.
-#' @param revgen If TRUE, use MT-TMCMC version 1, else version 2. Default is TRUE.
 #' @param burn_in The number of samples assigned as burn-in period. The default burn-in is taken to be one-third of nsamples.
 #' @param verb logical parameter, if TRUE the function prints the progress of simulation.
 #'
@@ -26,37 +24,29 @@
 #'  @import tmcmcR
 #'  @export
 #'
+#'
 
 
-mt_tmcmc_metrop <- function(target_pdf, scale, base, nmove_size, nmove, nsamples, revgen=TRUE, burn_in=NULL, verb=FALSE)
+
+mt_rwmh_metrop <- function(target_pdf, scale, base, nmove, nsamples, burn_in=NULL, verb=FALSE)
 {
   if(is.null(burn_in)) burn_in <- nsamples/3;
   chain <- matrix(0, nsamples, length(base))
   chain[1,] <- base;
   num <- 2
   while(num <= nsamples) {
-    eps <- rep(rnorm(nmove_size,0,scale), each=floor(nmove/nmove_size));
-    b <- t(sapply(1:nmove, function(l) sample(c(-1,+1),length(base),replace=TRUE)));
-    trial_chains <- chain[(num-1),]+ b*eps;
+    eps <- matrix(rnorm(nmove*length(base),0,scale), nrow=nmove);
+    trial_chains <- chain[(num-1),]+ eps;
     pi_trials <- unlist(lapply(1:nmove, function(x) target_pdf(trial_chains[x,])))
     pi_trials_norm <- exp(pi_trials - max(pi_trials));
     index <- sample(1:nmove, size=1,  prob=pi_trials_norm);
     move_candidate <- trial_chains[index,];
-    if(revgen){
-    b_rev <- t(sapply(1:(nmove-1), function(l) sample(c(-1,+1),length(base),replace=TRUE)));
-    eps_rev <- rep(c(rnorm((nmove_size-1),0,scale),abs(move_candidate[1]-chain[(num-1),1])), each=floor(nmove/nmove_size));
-    trial_chains_rev <- move_candidate + b_rev*eps_rev[1:(nmove-1)];
+    eps_rev <- rbind(matrix(rnorm((nmove-1)*length(base),0,scale), nrow=(nmove-1)),eps[index,]);
+    trial_chains_rev <- move_candidate + eps_rev;
     trial_chains_rev <- rbind(trial_chains_rev, chain[(num-1),]);
     pi_trials_rev <- unlist(lapply(1:nmove, function(x) target_pdf(trial_chains_rev[x,])))
     pi_trials_norm_rev <- exp(pi_trials_rev - max(pi_trials));
-    }
-    if(!revgen){
-      b_rev <- t(sapply(1:nmove, function(l) sample(c(-1,+1),length(base),replace=TRUE)));
-      b_rev[index,] <- -b[index,];
-      trial_chains_rev <- move_candidate + b_rev*eps;
-      pi_trials_rev <- unlist(lapply(1:nmove, function(x) target_pdf(trial_chains_rev[x,])))
-      pi_trials_norm_rev <- exp(pi_trials_rev - max(pi_trials));
-    }
+
     acc_rate <- min(1, sum(pi_trials_norm)/sum(pi_trials_norm_rev));
     if(runif(1,0,1) < acc_rate) {chain[num,] <- move_candidate;}
     else{ chain[num,] <- chain[(num-1),];}
@@ -74,4 +64,3 @@ mt_tmcmc_metrop <- function(target_pdf, scale, base, nmove_size, nmove, nsamples
   ll <- list("chain"=chain,"post.mean"=posterior_mean);
   return(ll)
 }
-
